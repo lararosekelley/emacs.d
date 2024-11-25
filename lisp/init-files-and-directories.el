49026775c8f90654bb824229a9413c4970878097
Lara Kelley <lararosekelley@gmail.com>
2024-11-25 11:35:13 -0500

    add files-and-directories missing file

---
+++ lisp/init-files-and-directories.el
@@ -0,0 +1,16 @@
+;;; init-files-and-directories.el --- Utils for basic file and directory operations
+;;;
+;;; Commentary:
+;;;   Author: @lararosekelley
+;;;   Futher reading:
+;;;     - https://github.com/nflath/sudo-edit
+;;;   Last modified: November 23rd, 2024
+;;; --------------------------------------------------------------------------------
+;;; Code:
+
+
+(use-package sudo-edit
+  :straight (:host github :repo "nflath/sudo-edit"))
+
+;;; init-files-and-directories.el ends here
+(provide 'init-files-and-directories)

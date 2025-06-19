e8b8bea8d2eab13ef679e3dbec46311c08766763
Lara Kelley <lararosekelley@gmail.com>
2025-06-19 01:49:31 -0400

    enable projectile in emacs-dashboard

--- lisp/init-dashboard.el
+++ lisp/init-dashboard.el
@@ -4,7 +4,7 @@
 ;;;   Author: @lararosekelley
 ;;;   Further reading:
 ;;;     - https://github.com/emacs-dashboard/emacs-dashboard
-;;;   Last modified: November 18th, 2024
+;;;   Last modified: June 19th, 2025
 ;;; -----------------------------------------
 ;;; Code:
 
@@ -14,15 +14,17 @@
   :init
   ;; Configure layout
   (setq dashboard-items '(
-    (recents   . 10)
-    (projects  . 5)
-    (agenda    . 5)
-    (bookmarks . 5)))
+                          (recents   . 10)
+                          (projects  . 5)
+                          (agenda    . 5)
+                          (bookmarks . 5)))
   ;; Title
   (setq dashboard-banner-logo-title "Hello, Lara")
   ;; Content is not centered by default. To center, set
   (setq dashboard-center-content t)
   (setq dashboard-vertically-center-content t)
+  ;; Show projects from projectile
+  (setq dashboard-projects-backend 'projectile)
   ;; Fancy icons
   (setq dashboard-display-icons-p t)
   (setq dashboard-icon-type 'nerd-icons)
